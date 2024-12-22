5a6b4a3042b69e6edca2e8af0cb5fb18d9bbdf89
Leixhik <eduardo.leijapl@uanl.edu.mx>
2024-12-22 15:50:06 -0600

    Hola

---
+++ IntroduccionJS/ejercicios/var1.js
@@ -0,0 +1,14 @@
+// Clase 104
+
+// 1. Hoisting: Declara una variable con var después de usarla en un console.log
+// ¿Qué sucede?
+var nombre = 'Pawish';
+console.log(nombre);
+nombre = 'Pakis';
+// R: El contenido de la variable cambió al nuevo pero el del console.log muestra el anterior.
+
+// 2. Ámbito de una función: Declara una variable con var dentro de un búcle for. Intenta
+// acceder  a ella fuera del buble. ¿Qué valor tiene?
+
+//hola
+
