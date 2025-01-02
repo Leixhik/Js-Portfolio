a51453075e34256041b4351ca537bc2847356125
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-01-02 11:46:13 -0600

    Clase 108 creada

---
+++ IntroduccionJS/ejercicios/clase108.js
@@ -0,0 +1,42 @@
+// Clase 108: Números y Operadores
+
+/*  1. Operaciones aritméticas básicas: Suma, resta, multiplicación, división y módulo */
+
+    let n1 = 5;
+    let n2 = 10;
+    console.log(`Operaciones con n1 = ${n1} y n2 = ${n2}:
+        suma = ${n1 + n2}
+        resta = ${n1 - n2}
+        multiplicación = ${n1 * n2}
+        división = ${n1 / n2}
+        módulo = ${n1 % n2}`);
+
+    /* R: He realizado diferentes operaciónes mediante operadores aritméticos utilizado
+    templates */
+//----------------------------------------------------------------------------------------
+
+/*  2.  Operadores de asignación: =, +=, -=, *=, /=, %= */
+
+    let num1 = 10;
+    let num2 = 15;
+    console.log(`Operaradores de asignación con num1 = ${num1} y num2 = ${num2}:
+        num1 = num2: ${num1 = num2}
+        num1 += num2: ${num1 += num2}
+        num1 -= num2: ${num1 -= num2}
+        num1 *= num2: ${num1 *= num2} 
+        num1 /= num2: ${num1 /= num2}
+        num1 %= num2: ${num1 %= num2}`)
+
+    /* R: Los operadores de asignación nos ayudan a proporcionar valores ya sean nuevos o 
+    sobre algun otro valor, es decir, tomando como base el valor de alguna otra variable.*/
+//----------------------------------------------------------------------------------------
+
+/*  3. Operadores de incremento / decremento: ++, -- */
+
+    let n3 = 2;
+    let n4 = 4;
+    console.log(`Numero 1 = ${n3} , Incremento ++ = ${++n3}
+        Numero 2 = ${n4}, Decremento -- = ${--n4}`);
+
+    /* R: En esta operación utilizamos el ++ y -- antes de la variable para que pueda
+    mostrar los cambios en consola.  */
