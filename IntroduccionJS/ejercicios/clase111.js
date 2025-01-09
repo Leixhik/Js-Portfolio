8280dd82f0f67b7a3c9fdccac083ff8ebaecba1c
Rodolfo Leija <leija_26010@hotmail.com>
2025-01-09 15:04:30 -0600

    He terminado la clase 111, con concatenaciones y template strings, pronto crearé la clase 112.

--- IntroduccionJS/ejercicios/clase111.js
+++ IntroduccionJS/ejercicios/clase111.js
@@ -71,7 +71,10 @@ con " es mi favorito". */
     console.log(frase);
 
     /* R: Pienso que esta algo equivocado mi ejercicio, pues tengo diferentes
-    tipos de datos en una variable. */
+    tipos de datos en una variable. Esto no es incorrecto. En JavaScript, 
+    una variable puede contener valores de diferentes tipos a lo largo 
+    de su vida (aunque no es una práctica recomendada en muchos casos, 
+    es posible). */
 //----------------------------------------------------------------------------------------
 
 /* 8. Dados texto1 = "Esto es" y texto2 = "una prueba", concaténalos con
@@ -100,6 +103,102 @@ un espacio y un punto al final. */
     /* R: El resultado es 123, pues no realiza ninguna operación, sólo concatena*/
 //----------------------------------------------------------------------------------------
 
-/*  Templates */
+/*                                      Templates                                        */
 
-/*  1.  */
+/*  1. Usa template strings para crear el saludo "Hola,[nombre]", donde [nombre] 
+es una variable.*/
+
+    let miNombre = "Rodolfo";
+    console.log(`Hola ${miNombre}`);
+
+    /* R: Usando templates utilizo el ${} para agregar mi variable. */
+//----------------------------------------------------------------------------------------
+
+/*  2. Crea una frase usando template strings que incluya tu nombre, edad y
+ciudad. */
+
+    let ciudad = 'Apodaca';
+    console.log(`Nombre: ${miNombre}, Edad: ${edad}, Ciudad: ${ciudad}`);
+
+    /* R: Dado que ya tengo algunas variables definidas, sólo me hizo falta
+    concatenarlas. */
+//----------------------------------------------------------------------------------------
+
+/*  3. Con template strings, muestra el resultado de la multiplicación
+de 5 x 7. */
+
+    console.log(`Resultado de 5 x 7 = ${5 * 7}`);
+    /* R: Utilicé el ${}d para realizar ahi mismo la operación. */
+//----------------------------------------------------------------------------------------
+
+/*  4. Tienes las variables producto = "manzanas" y precio = 2.5. Usa template
+strings para mostrar "El precio de las manzanas es $2.5 */
+
+    let producto = 2.5;
+    console.log(`El precio de las manzanas es $${producto}`);
+
+    /* R: Se ha realizado el cambio. */
+//----------------------------------------------------------------------------------------
+
+/*  5. Crea una template string que muestre la tabla de multiplicar del 3
+hasta el 5, usando un bucle for. */
+
+    for(let i = 0 ; i < 5 ; i++){
+        let tabla = 3 * (i + 1);
+        console.log(`3 x ${i +1} = ${tabla}`);
+    }
+    
+    /* R: Utilizando un bucle for puse un límite hasta 5, después tomé
+    el valor de i para mostrar el conteo y además realizar con un +1 
+    la multiplicación. */
+//----------------------------------------------------------------------------------------
+
+/*  6. Con variables autor = "Gabriel García Marquez" y libro = "Cien años
+de soledad", crea una frase usando template strings. */
+
+    let autor = "Gabriel García Márquez";
+    let libro = "Cien años de Soledad";
+    console.log(`Autor: ${autor}
+        Libro: ${libro}`);
+    
+    /* R: Agregué un formato con template strings. */
+//----------------------------------------------------------------------------------------
+
+/*  7. Usa template strings para mostrar "El área de un cuadrado de lado 4 es 
+16." */
+
+    console.log(`El área de un cuadrado de lado 4 es: ${Math.pow(4,2)}`);
+
+    /* R: Usé Math.pow para elevar al cuadrado el valor 4 y obtener 16 dentro
+    del template string. */
+//----------------------------------------------------------------------------------------
+
+/*  8. Si nombre = "Ana" y trabajo = "programadora", crea una frase con 
+template strings. */
+
+    let nombre1 = "Ana";
+    let trabajo1 = "programadora";
+    console.log(`Mi nombre es ${nombre1} y soy ${trabajo1}`);
+
+    /* R: No hay mucho que decir, solo creé una frase con dos variables en
+    el template. */
+//----------------------------------------------------------------------------------------
+
+/*  9. Crea una template string que muestre la suma de dos variables num1 y num2.*/
+
+    let num1 = 10;
+    let num2 = 20;
+    console.log(`La suma de num1 y num2 es: ${num1 + num2}`);
+
+    /* R: Ejercicio práctico y sencillo. */
+//----------------------------------------------------------------------------------------
+
+/*  10. Con template strings, crea una lista numerada del 1 al 3. */
+
+    console.log(`Lista:
+        1. 
+        2. 
+        3. `);
+    
+    /* R: Entendí esta forma de crear la lista. */
+    
