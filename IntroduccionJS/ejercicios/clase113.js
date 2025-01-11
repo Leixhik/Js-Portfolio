954e40a5b568ad6ebb40004f743d6673e6437e8a
Rodolfo Leija <leija_26010@hotmail.com>
2025-01-10 18:58:40 -0600

    Clase 113 empezada

---
+++ IntroduccionJS/ejercicios/clase113.js
@@ -0,0 +1,17 @@
+// Clase 113: Objetos en JavaScript
+
+/*  1. Crea un objeto llamado persona con las propiedades nombre, edad y profesión.
+Asigna valores a estas propiedades. */
+
+    const persona = {
+        nombre:  "Rodolfo",
+        edad: 24,
+        profesion: "Programador"
+    }
+    console.log(persona);
+
+//----------------------------------------------------------------------------------------+
+
+/* 2. Accede a las propiedades del objeto persona creado en el ejercicio 1 y muestra
+sus valores en la consola. Usa tanto la notación de punto (objeto.propiedad) como
+la notación de corchetes (objeto['propiedad']). */
