83667cbc7dec2af8243f56fb1693b34c8c74c99b
Rodolfo Leija <leija_26010@hotmail.com>
2025-01-12 21:42:48 -0600

    he avanzado la clase 113

--- IntroduccionJS/ejercicios/clase113.js
+++ IntroduccionJS/ejercicios/clase113.js
@@ -12,6 +12,58 @@ Asigna valores a estas propiedades. */
 
 //----------------------------------------------------------------------------------------+
 
-/* 2. Accede a las propiedades del objeto persona creado en el ejercicio 1 y muestra
+/*  2.  Accede a las propiedades del objeto persona creado en el ejercicio 1 y muestra
 sus valores en la consola. Usa tanto la notación de punto (objeto.propiedad) como
 la notación de corchetes (objeto['propiedad']). */
+
+    console.log(persona.nombre);
+    console.log(persona['profesion']);
+    console.log(`Hola mi nombre es ${persona.nombre}, tengo ${persona.edad} años y
+        soy ${persona['profesion']}.`);
+//----------------------------------------------------------------------------------------+
+
+/*  3. Añade una nueva propiedad llamada ciudad al objeto persona. */
+
+    persona.ciudad = 'Apodaca';
+    console.log(persona['ciudad']);
+//----------------------------------------------------------------------------------------+
+
+/*  4. Modifica el valor de la propiedad edad del objeto persona.*/
+
+    persona.edad = '25';
+    console.log(persona.edad);
+
+//----------------------------------------------------------------------------------------+
+
+/*  5. Crea un objeto llamado libro con las propiedades titulo, autor y
+añoPublicacion. */
+    
+    const Libro = {
+        titulo: 'El nuevo Dios',
+        autor: 'Jose Luis Chapa',
+        añoPublicacion: 2025
+    }
+
+//----------------------------------------------------------------------------------------+
+
+/* 6. Crea un objeto llamado rectangulo con las propiedades ancho y alto. Agrega un
+método llamado calcularArea que calcule y retorne el área del rectángulo 
+(área = ancho * alto) */
+
+    const Rectangulo = {
+        ancho: 15,
+        alto: 10
+    }
+
+    function calcularArea (){
+        area = (Rectangulo.alto * Rectangulo.ancho);
+        return area;
+    }
+    console.log(calcularArea());
+//----------------------------------------------------------------------------------------+
+
+/*  7. Crea un array llamado estudiantes que contenga tres objetos.
+Cada objeto debe representar a un estudiante con las propiedades nombre y
+calificación. */
+
+    
