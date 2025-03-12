cc201d794a99b4848843de9a47c4e4b55aa4db36
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-03-12 16:44:51 -0600

    He terminado la lógica de Js, empiezo a ver el DOM

--- IntroduccionJS/js/33.js
+++ IntroduccionJS/js/33.js
@@ -1,11 +1,19 @@
-function obtenerEmpleados(){
-    
-    const archivo = "empleados.json";  // Guardé la URL de mi conexión a un const
+async function obtenerEmpleados() {
 
-    fetch(archivo) // Aquí iria el URL de la API
-        .then( resultado => { //fetch utiliza promises
-            console.log(resultado)
-    });
+    const archivo = "js/empleados.json";  // Guardé la URL de mi conexión a un const
+
+    // fetch(archivo) // Aquí iria el URL de la API
+    //     .then(resultado =>  /*fetch utiliza promises*/ resultado.json())
+    //     .then(datos => {
+    //         // console.log(datos.empleados);
+
+    //         const { empleados } = datos; // Forma: creas una variable, le almacenas la traducción del .json y eso te permite hacer más fácil la cosas
+    //         console.log(empleados); 
+    //     });
+
+    const resultado = await fetch(archivo);
+    const datos = await resultado.json();
+    console.log(datos)
 }
 
 obtenerEmpleados();
